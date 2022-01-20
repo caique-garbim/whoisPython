428aecc63442654786514218b2736fee66d7501b
Caique Garbim <76706456+caiquegarbim01@users.noreply.github.com>
2022-01-20 00:00:06 -0300

    Create whois_python.py

---
+++ whois_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+#
+# Especifique o site como argumento
+# Exemplo: python whois.py site.com.br
+#
+
+import socket,sys
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("whois.iana.org",43))
+s.send(sys.argv[1]+"\r\n")  
+resposta = s.recv(1024).split()
+whois = resposta[19]
+s.close() 
+
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.connect((whois,43))
+s1.send(sys.argv[1]+"\r\n")
+resp = s1.recv(2048)
+print resp
